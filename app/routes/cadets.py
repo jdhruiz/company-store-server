f22b55bff2c3d1bfc8f667744e0ce2f658bdaa55
jdhruiz <james.ruiz@westpoint.edu>
2026-02-11 21:24:58 -0500

    Fixed cadets.py

--- app/routes/cadets.py
+++ app/routes/cadets.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from db import get_connection
 from utils import query_db
 
 cadets_bp = Blueprint("cadets", __name__)
@@ -7,3 +8,26 @@ cadets_bp = Blueprint("cadets", __name__)
 def get_cadets():
     data = query_db("SELECT * FROM cadets;")
     return jsonify(data)
+
+@cadets_bp.route("/", methods=["POST"])
+def add_cadet():
+    data = request.json
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        INSERT INTO cadets (first_name, last_name, company, class_year)
+        VALUES (%s, %s, %s, %s)
+    """, (
+        data["first_name"],
+        data["last_name"],
+        data["company"],
+        data["class_year"]
+    ))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return {"message": "Cadet added"}, 201
