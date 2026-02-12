33d1898fb8c4098734d465ece07b6e2418fedc33
jdhruiz <james.ruiz@westpoint.edu>
2026-02-11 21:20:14 -0500

    Fixed orders.py

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -1,13 +1,7 @@
 from flask import Blueprint, request, jsonify
 from db import get_connection
-from utils import query_db
 
 orders_bp = Blueprint("orders", __name__)
-
-@orders_bp.route("/", methods=["GET"])
-def get_orders():
-    data = query_db("SELECT * FROM orders;")
-    return jsonify(data)
 
 @orders_bp.route("/", methods=["POST"])
 def create_order():
@@ -16,3 +10,27 @@ def create_order():
     conn = get_connection()
     cur = conn.cursor()
 
+    # Insert order
+    cur.execute("""
+        INSERT INTO orders (cadet_id)
+        VALUES (%s)
+        RETURNING id
+    """, (data["cadet_id"],))
+
+    order_id = cur.fetchone()[0]
+
+    # Insert order items
+    for item in data["items"]:
+        cur.execute("""
+            INSERT INTO order_items (order_id, item_id, quantity)
+            VALUES (%s, %s, %s)
+        """, (order_id, item["item_id"], item["quantity"]))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return jsonify({
+        "message": "Order created",
+        "order_id": order_id
+    }), 201
